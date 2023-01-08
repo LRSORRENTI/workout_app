b21ce8fc16f3b281ab4edb92e4b847b58baaa091
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-01-08 17:31:15 -0600

    added components folder, and added first component, the navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+//Quick note to self, running rafce is clutch, it 
+//will auto generate the component
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
