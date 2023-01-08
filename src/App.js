6246e86e7b7a1f801a319301e84c48ca82c208b2
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-01-08 17:18:22 -0600

    added pictures for exercise components, also added imports to main app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { Route, Routes} from 'react-router-dom';
+
+//The above will be used to route between the 
+//home-page and the exercise details page
+
+import { Box } from '@mui/material'
+
+// this is a div with some colors / shading from 
+//material ui
+
+import './App.css';
+
 const App = () => {
   return (
-    <div>App</div>
+    <Box>App</Box>
   )
 }
 
