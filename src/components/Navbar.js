68d92a5ab3cbe4278e453ce85629635edd9cedea
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-01-12 09:12:43 -0600

    added imports for link and stack components from MUI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
 //Quick note to self, running rafce is clutch, it 
 //will auto generate the component
 
-import React from 'react'
+import React from 'react';
+
+import {Link } from 'react-router-dom';
+
+import { Stack } from '@mui/material';
+
+//The Stack component manages layout of i
+// mmediate children along the vertical 
+// or horizontal axis with optional spacing
+//  and/or dividers between each child.
+
+//Stacks are very useful for creating 
+//horizontal or vertical lists 
+
+
+//above will link the link from react router dom, 
+//and the stack from mui kit, these will help 
+//build visually appealing components quickly
 
 const Navbar = () => {
   return (
