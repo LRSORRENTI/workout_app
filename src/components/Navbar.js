5059170c96f135cccf0e729efe773ed04523cb35
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-01-12 09:26:01 -0600

    Added additional link to navbar component, will link to exercises later

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,8 +27,18 @@ const Navbar = () => {
   return (
     <Stack>
       <Link to="/">
-      <img src={Logo}/>
+      <img src={Logo} alt="logo" style={{
+        width: '48px',
+         height: '48px', 
+        margin: '0 20px'
+      }}/>
       </Link>
+      <Stack>
+        <Link to="/">Home</Link>
+        <a href="#exercises" 
+        style={{textDecoration:'none',
+        color: '#3A1212'}}>Exercises</a>
+      </Stack>
     </Stack>
   )
 }
