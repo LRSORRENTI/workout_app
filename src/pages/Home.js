ac062248ddf661fd309954f44c458e87cecd61c6
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-01-12 09:53:44 -0600

    created component files for displaying hero content as well as search and exercises in components folder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+//useState will help us with some logic
+import {Box } from '@mui/material';
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
